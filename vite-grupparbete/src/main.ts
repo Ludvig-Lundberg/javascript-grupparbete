7075e03e8a2e70dd6b8497e42ee57fc1f1f159b3
Ludvig-Lundberg <ickeskola@gmail.com>
2022-12-19 11:49:25 +0100

    lagt till knapp som lägger till TILLFÄLLIG object i 'cart' array:en

--- vite-grupparbete/src/main.ts
+++ vite-grupparbete/src/main.ts
@@ -1,2 +1,15 @@
 import './style.css'
 
+let cart: object [] = [];
+
+let button = document.querySelector("#addButton");
+
+button?.addEventListener("click", e => {
+    cart.push({
+        produkt: "banan",
+        quantity: 2,
+        cost: 10
+    })
+    console.log(cart.length);
+    console.log(cart);
+})

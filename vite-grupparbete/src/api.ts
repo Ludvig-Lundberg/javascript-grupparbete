910170c609ffcd271dc529c1d2e9fe70f08100be
Arden <arden.haldorson@gmail.com>
2022-12-19 11:36:14 +0100

    Added separate file for api, fetch function, imp/e

---
+++ vite-grupparbete/src/api.ts
@@ -0,0 +1,13 @@
+import { IItem } from './interfaces'
+
+import './style.css'
+
+export const fetchItems = async () => {
+	const res = await fetch("https://bortakvall.se/api/products")
+
+	if (!res.ok) {
+		throw new Error(`${res.status} ${res.statusText}`)
+	}
+
+	return await res.json() as IItem
+}

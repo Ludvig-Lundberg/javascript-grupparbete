10611ad80108898376902487ccc17f85205b75a7
Arden <arden.haldorson@gmail.com>
2022-12-19 11:24:12 +0100

    Created an interface for items in the products arr

---
+++ vite-grupparbete/src/interfaces.ts
@@ -0,0 +1,13 @@
+export interface IItem {
+    id: number,
+    name: string,
+    description: string,
+    price: number,
+    on_sale: boolean,
+    images: {
+        thumbnail: string,
+        large: string
+    },
+    stock_status: string,
+    stock_quantity: string
+  }

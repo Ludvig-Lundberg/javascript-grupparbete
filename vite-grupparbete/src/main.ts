42a0ff4650cd53d4652bfa3e7213c4c51783cde7
Arden <arden.haldorson@gmail.com>
2022-12-19 15:13:07 +0100

    added buttons to show and close info div

--- vite-grupparbete/src/main.ts
+++ vite-grupparbete/src/main.ts
@@ -4,8 +4,10 @@ import {fetchItems} from './api'
 const moreInfo = document.querySelector("#more-information")
 const moreInfoText = document.querySelector("#info-text")
 const infoDiv = document.querySelector("#fade-background")
+const infoBtn = document.querySelector("#info-btn")
+const closeInfoBtn = document.querySelector("#close-info-btn")
 
-// array för att hämta item från API
+// array för att hämta item (product) från API
 let items : IItem[] = []
 
 import './style.css'
@@ -24,4 +26,26 @@ button?.addEventListener("click", e => {
 })
 // Temporär knapp
 
-moreInfoText!.innerHTML = `<p>Hej hej hej</p>`
+//testing
+moreInfoText!.innerHTML = `
+    <h3>Title</h3> 
+    <span class="price">Pris: 20</span>
+    <p>Hej hej hej</p>`
+
+infoBtn?.addEventListener("click", () => {
+    infoDiv!.classList.toggle("d-none")
+})
+
+infoDiv?.addEventListener("click", e => {
+
+    const target = e.target as HTMLElement
+
+    if (target.tagName === "DIV") {
+        infoDiv!.classList.toggle("d-none")
+    }
+
+})
+
+closeInfoBtn?.addEventListener("click", () => {
+    infoDiv!.classList.toggle("d-none")
+})

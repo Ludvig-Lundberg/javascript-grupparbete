35ca5bf466a80af1ec3d6344883feb2cc9e57303
Ludvig-Lundberg <ickeskola@gmail.com>
2022-12-30 14:06:35 +0100

    added "snap-like" scrolling to the header added animation.

---
+++ vite-grupparbete/src/intro.ts
@@ -0,0 +1,13 @@
+export {};
+
+const headerEl = document.querySelector("header")!,
+      navbarEl = document.querySelector("#navbar")!;
+let viewportHeight = window.innerHeight;
+window.addEventListener("scroll", () => {
+    let navbarBounding = navbarEl.getBoundingClientRect();
+    if (navbarBounding.top <= (viewportHeight - 20) && navbarBounding.top >= (viewportHeight - 50)) {
+        headerEl.classList.add("headerIntro")
+        document.body.scrollTop = document.documentElement.scrollTop = 0;
+
+    }
+})
